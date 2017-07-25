18fe2ec4e27e98f2c61f03d6a20fb0077c072562
Nikos Katsos <nikoskatsos101@gmail.com>
2017-07-25 22:30:40 +0300

    created player

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float startingSize;
+
+	void Awake() {
+		transform.localScale = new Vector3(startingSize, startingSize, 0);
+	}
+
+}
