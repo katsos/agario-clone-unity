bfe99169df1d3c5ac0341c5363582f9d11c37a07
Nikos Katsos <nikoskatsos101@gmail.com>
2017-07-27 20:16:17 +0300

    camera follows player smoothly

---
+++ Assets/scripts/FollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+	public float smoothSpeed;
+	float cameraZposition;
+	GameObject player;
+	Vector3 playerPosition;
+	void Awake() {
+		player = GameObject.Find("player");
+		cameraZposition = transform.position.z;
+	}
+	
+	void LateUpdate() {
+		followPlayer ();
+	}
+
+	/**
+	 * Follow player on x and y axis
+	 */
+	void followPlayer() {
+		Vector3 playerPosition = player.transform.position;
+		Vector3 whereToGo = new Vector3(playerPosition.x, playerPosition.y, cameraZposition);
+		transform.position = Vector3.Lerp (transform.position, whereToGo, smoothSpeed);
+	}
+}
