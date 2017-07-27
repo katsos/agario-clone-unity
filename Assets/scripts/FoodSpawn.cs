4f19ed03ae528d6a7a1c32eeef9417e370fc5904
Nikos Katsos <nikoskatsos101@gmail.com>
2017-07-27 19:14:05 +0300

    Added dummy food spawner script into game manager

---
+++ Assets/scripts/FoodSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawn : MonoBehaviour {
+
+	public GameObject food;
+	public float speed;
+
+	void Start () {
+		// InvokeRepeating("Spawn", 0, speed);
+		Spawn();
+	}
+	
+	void Spawn () {
+		Vector3 position = new Vector3(4, 4, 0);
+		Quaternion noRotation = Quaternion.identity;
+		Instantiate(food, position, noRotation);
+	}
+}
